40ba1508cac297d6f50fe07c4a35e31e105eb1a0
naveensinghpanwa <46571841+naveensinghpanwa@users.noreply.github.com>
2025-01-09 10:25:10 +0530

    Update L_01_MergeSorting.java

--- src/DAA_06_Sorting/L_01_MergeSorting.java
+++ src/DAA_06_Sorting/L_01_MergeSorting.java
@@ -1,30 +1,54 @@
-package DAA_06_Sorting;
+import java.util.Arrays;
 
 public class L_01_MergeSorting {
     public static void main(String[] args) {
-        int[] a = {10, 15, 20};
-        int[] b = {5, 6, 6, 15};
-
+        int[] al = {10, 20, 40 ,20, 30};
+        mergeSort(al, 0, al.length - 1);
+        Arrays.stream(al).asLongStream().forEach(System.out::println);
     }
 
-    private static void mergeSort(int[] a, int[] b) {
-        int[] result = new int[a.length + b.length];
-
+    private static void mergeSort(int[] a, int start, int end) {
+        if (start < end) {
+            int mid = start + (end - start) / 2;  // todo: important to prevent overflow
+            mergeSort(a, start, mid);
+            mergeSort(a, mid + 1, end);
+            mergeProcedure(a, start, mid, end);
+        }
     }
+    private static void mergeProcedure(int []a, int start, int mid, int end) {
+        int[] l = new int[mid - start + 1];
+        int[] r = new int[end - mid];
 
-    private static void merge(int[] arr, int l, int m, int r) {
-   /*     int leftTracker = l;
-        int rightTracker = m;
-        while (l <= m && ) {
+        for (int i = 0; i < l.length; i++) {
+            l[i] = a[i + start];
+        }
 
-        }*/
+        for (int j = 0; j < r.length; j++) {
+            r[j] = a[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = start;
+        while (i < l.length && j < r.length){
+            if (l[i] <= r[j]) {
+                a[k] = l[i];
+                i++;
+            } else {
+                a[k] = r[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < l.length) {
+            a[k] = l[i];
+            k++;
+            i++;
+        }
+
+        while (j < r.length) {
+            a[k] = r[j];
+            k++;
+            j++;
+        }
     }
 }
-
-/*
-
- {5, 10, 30}
- {2, 7, 15}
-
- 2, 5, 7, 10, 15, 30
-*/
